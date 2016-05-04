84f2d0948012a005a9be020fe1251b7d5a48d446
MrAnyone <m3rc3r99@gmail.com>
2016-05-04 11:20:26 -0300

    Remove a console message

--- injector_listeners.js
+++ injector_listeners.js
@@ -3,7 +3,6 @@
 //actions
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(request);
         window.postMessage({
             action: request.action,
             content: request.data
