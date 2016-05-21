9b472b88586c85779d56d52d005c958d28058224
MrAnyone <m3rc3r99@gmail.com>
2016-05-21 15:22:18 -0300

    Change some listeners location

--- src/listeners/ytav_listeners.js
+++ src/listeners/ytav_listeners.js
@@ -14,3 +14,23 @@ window.addEventListener("message", function(event) {
             break;
     }
 }, false);
+
+//startup the visualizer
+$(document).ready(function() {
+    vis = new audioVisualizer(640, 360, "#player", "video", "#player-api");
+    vis.init();
+    vis.render();
+
+    //a listener to check if a new video has been loaded
+    //if yes, focus on it
+    $(document).bind("DOMNodeInserted", function(e) {
+        //checks for the video time current time text
+        //it is better to use this element because
+        //it is added every time
+        if ($(e.target).get(0) == $("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div > span.ytp-time-current").get(0)) {
+            scrollToVideo();
+            //unbind to prevent unused processing
+            $(document).unbind("DOMNodeInserted");
+        }
+    });
+});
