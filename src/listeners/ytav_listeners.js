3e0dfacab7c6091a7b128d8a59ddcc9da9dbcb30
MrAnyone <m3rc3r99@gmail.com>
2016-05-21 15:54:39 -0300

    Remove unnused document.ready listener

--- src/listeners/ytav_listeners.js
+++ src/listeners/ytav_listeners.js
@@ -16,20 +16,19 @@ window.addEventListener("message", function(event) {
 }, false);
 
 //startup the visualizer
-$(document).ready(function() {
-    //a listener to check if a new video has been loaded
-    //if yes, focus on it
-    $(document).bind("DOMNodeInserted", function(e) {
-        //checks for the video time current time text
-        //it is better to use this element because
-        //it is added every time
-        if ($(e.target).get(0) == $("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div > span.ytp-time-current").get(0)) {
-            //unbind to prevent unused processing
-            $(document).unbind("DOMNodeInserted");
-            vis = new audioVisualizer(640, 360, "#player", "video", "#player-api");
-            vis.init();
-            vis.render();
-            setTimeout(scrollToVideo, 1000);
-        }
-    });
+
+//a listener to check if a new video has been loaded
+//if yes, load the canvas & focus on the video
+$(document).bind("DOMNodeInserted", function(e) {
+    //checks for the video time current time text
+    //it is better to use this element because
+    //it is added every time
+    if ($(e.target).get(0) == $("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div > span.ytp-time-current").get(0)) {
+        //unbind to prevent unused processing
+        $(document).unbind("DOMNodeInserted");
+        vis = new audioVisualizer(640, 360, "#player", "video", "#player-api");
+        vis.init();
+        vis.render();
+        setTimeout(scrollToVideo, 1000);
+    }
 });
