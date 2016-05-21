ebb89ffd536a1d06f271a64becb5735631b9b5b3
MrAnyone <m3rc3r99@gmail.com>
2016-05-21 15:20:12 -0300

    :tada: :bug: Change how the visualizer is loaded
    The other way was making the canvas doesn't appears when it supposed to.

--- src/ytav.js
+++ src/ytav.js
@@ -428,13 +428,29 @@ function audioVisualizer(width, height, containerSelector, sourceSelector, playe
 
 }
 
+//used to focus on the YT video
+function scrollToVideo() {
+        $("html, body").animate({
+            scrollTop: $("#movie_player > div.ytp-iv-video-content").offset().top - $("#player > canvas").offset().top
+        }, 500);
+}
+
+//startup the visualizer
 $(document).ready(function() {
     vis = new audioVisualizer(640, 360, "#player", "video", "#player-api");
     vis.init();
     vis.render();
-    setTimeout(function() {
-        $("html, body").animate({
-            scrollTop: $("#movie_player > div.ytp-iv-video-content").offset().top - $("#player > canvas").offset().top
-        }, 500);
-    }, 1000);
+
+    //a listener to check if a new video has been loaded
+    //if yes, focus on it
+    $(document).bind("DOMNodeInserted", function(e) {
+        //checks for the video time current time text
+        //it is better to use this element because
+        //it is added every time
+        if ($(e.target).get(0) == $("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div > span.ytp-time-current").get(0)) {
+            scrollToVideo();
+            //unbind to prevent unused processing
+            $(document).unbind("DOMNodeInserted");
+        }
+    });
 });
